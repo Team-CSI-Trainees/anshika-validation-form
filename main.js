3407fb573ab03b814745658d75bccaa7a3e82462
lilysinghania <113258933+lilysinghania@users.noreply.github.com>
2022-10-13 02:10:30 +0530

    third commit

--- main.js
+++ main.js
@@ -22,33 +22,37 @@ form.addEventListener("submit", (e) => {
   engine(username, 0, "Username cannot be blank");
   engine(email, 1, "Email cannot be blank");
   engine(phonenumber, 2, "phone number cannot be blank");
-  engine(password, 3, "Password cannot be blank");
-  engine(confirmpassword, 4, "confirm password cannot be blank");
+  engine(password, 4, "Password cannot be blank");
+  engine(confirmpassword, 5, "confirm password cannot be blank");
 });
 
 // Adding the checking event listener
 
-// let check = document.querySelector(".check");
-// let number = document.querySelector(".phone number");
-// let text = document.querySelector(".text");
+ let check = document.querySelector(".check");
+ let number = document.querySelector(".phone number");
+let containerh5 = document.querySelector(".container h5");
 
-// let regex = /^[\d,\s,\+,\-]{5,20}/;
+let regex = /^[\d,\s,\+,\-]{5,20}/;
 
-// check.addEventListener("click",()=>{
-// 	if(phone number.value ==""){
-// 		text.innerText = "Please Enter Your Phone Number";
-// 		text.style.color = "#fff";
-// 	}
-// 	else if(phone number.value.match(regex)){
-// 		text.innerText = "Congrats! You Enter A Valid Phone Number";
-// 		text.style.color = "rgba(4,125,9,1)";
-// 	}
-// 	else
-// 		{
-// 		text.innerText = "Oops! Your Phone Number Is Invalid";
-// 		text.style.color = "#da3400";
-// 		}
-// });
+check.addEventListener("click",()=>{
+	if(phonenumber.value ==""){
+		containerh5.innerText = "Please Enter Your Phone Number";
+		containerh5.style.color = "#fff";
+	}
+	else if(phonenumber.value.match(regex)){
+		containerh5.innerText = "Congrats! You Enter A Valid Phone Number";
+		containerh5.style.color = "rgba(4,125,9,1)";
+	}
+	else
+		{
+		containerh5.innerText = "Oops! Your Phone Number Is Invalid";
+		containerh5.style.color = "#da3400";
+        // phonenumber.style.border = "2px solid red";
+        // failureIcon[2].style.opacity = "1";
+        // successIcon[2].style.opacity = "0";
+
+		}
+});
 
 
 
